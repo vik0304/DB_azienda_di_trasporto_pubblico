f984ec78eafb28a534edd58d31c52d9664525b94
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-09-29 15:01:29 +0200

    Update Application.java

--- src/main/java/team6/Application.java
+++ src/main/java/team6/Application.java
@@ -1,8 +1,21 @@
 package team6;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Scanner;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("dbtrasporti");
+    static Scanner s = new Scanner(System.in);
 
     public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
         System.out.println("Hello World!");
+        
+        s.close();
+        em.close();
+        emf.close();
     }
 }
