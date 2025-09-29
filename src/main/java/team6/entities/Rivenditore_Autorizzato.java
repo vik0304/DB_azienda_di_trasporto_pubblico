46158348be0f5da86267ff4cc05e23abcf3027fb
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-09-29 12:52:36 +0200

    temporary classes

--- src/main/java/team6/entities/Rivenditore_Autorizzato.java
+++ src/main/java/team6/entities/Rivenditore_Autorizzato.java
@@ -1,4 +1,10 @@
 package team6.entities;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Rivenditore_Autorizzato {
+    private String nome;
+
+
 }
--- src/main/java/team6/entities/Venditore.java
+++ src/main/java/team6/entities/Venditore.java
@@ -6,9 +6,15 @@ import java.util.UUID;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class Venditore {
+public abstract class Venditore {
     @Id
     @GeneratedValue
     protected UUID id;
+    protected String position;
 
+    public Venditore(){}
+
+    public Venditore(String position){
+        this.position=position;
+    }
 }
