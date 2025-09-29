35b3f10317e983fd109dd8617b407b0f7b70ca6a
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-09-29 10:24:44 +0200

    Initial commit

---
+++ src/main/java/team6/Application.java
@@ -0,0 +1,8 @@
+package team6;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/team6/ApplicationTest.java
@@ -0,0 +1,13 @@
+package team6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
