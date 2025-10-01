0831744d75941120e72b84bc5a591303da5bd5fd
KevR98 <ramil.kevin98@gmail.com>
2025-10-01 11:41:10 +0200

    query of the traveltime and numberoftravel

--- src/main/java/team6/dao/PercorrenzaDAO.java
+++ src/main/java/team6/dao/PercorrenzaDAO.java
@@ -2,10 +2,14 @@ package team6.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Parameter;
+import jakarta.persistence.TypedQuery;
 import team6.entities.Percorrenza;
+import team6.entities.Tratta;
 import team6.entities.Venditore;
 import team6.exeptions.NotFoundException;
 
+import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -51,4 +55,21 @@ public class PercorrenzaDAO {
     public List<Percorrenza> findByDataPartenza(LocalDate dataPartenza) { return null; }
 
     public List<Percorrenza> findByDateRange(LocalDate startDate, LocalDate endDate) { return null; }
+
+    public long numPercorrenza(UUID trattaId){
+        TypedQuery<Long> query = entityManager.createQuery(
+                "SELECT COUNT(p) FROM Percorrenza WHERE tratta_id = :trattaId", Long.class
+        );
+        query.setParameter("trattaId", trattaId);
+        return query.getSingleResult();
+    }
+
+    public long tempoEffettivoTratta(UUID trattaid){
+        TypedQuery<Long> query = entityManager.createQuery(
+                "SELECT SUM(FUNCTION('TIMESTAMPDIFF', MINUTE, p.dataPartenza, p.dataArrivo)) FROM Percorrenza p WHERE p.tratta.id = :trattaId",
+                Long.class
+        );
+        query.setParameter("trattaId", trattaid);
+        return query.getSingleResult();
+    }
 }
